59d2fb1b5528453c69ad592866b0a504c6f2c934
yoninsight <yo2120018@gmial.com>
2024-08-21 22:22:17 +0300

    Update

--- notebooks/__init__.py
+++ notebooks/__init__.py
@@ -52,6 +52,56 @@ plt.plot(togo_df['Timestamp'], togo_df['GHI'], label='Togo GHI')
 plt.legend()
 
 plt.show()
+# Correlation matrix for Benin
+print("Benin Correlation Matrix:")
+print(benin_df.corr())
+
+# Correlation matrix for Sierra Leone
+print("\nSierra Leone Correlation Matrix:")
+print(sierraleone_df.corr())
+
+# Correlation matrix for Togo
+print("\nTogo Correlation Matrix:")
+print(togo_df.corr())
+
+# Visualizing the correlation matrix
+plt.figure(figsize=(14, 7))
+
+plt.subplot(3, 1, 1)
+sns.heatmap(benin_df.corr(), annot=True)
+plt.title('Benin Correlation Matrix')
+
+plt.subplot(3, 1, 2)
+sns.heatmap(sierraleone_df.corr(), annot=True)
+plt.title('Sierra Leone Correlation Matrix')
+
+plt.subplot(3, 1, 3)
+sns.heatmap(togo_df.corr(), annot=True)
+plt.title('Togo Correlation Matrix')
+
+plt.show()
+# Convert Timestamp to datetime
+benin_df['Timestamp'] = pd.to_datetime(benin_df['Timestamp'])
+sierraleone_df['Timestamp'] = pd.to_datetime(sierraleone_df['Timestamp'])
+togo_df['Timestamp'] = pd.to_datetime(togo_df['Timestamp'])
+
+# Plotting time series for GHI
+plt.figure(figsize=(14, 7))
+
+plt.subplot(3, 1, 1)
+plt.plot(benin_df['Timestamp'], benin_df['GHI'], label='Benin GHI')
+plt.legend()
+
+plt.subplot(3, 1, 2)
+plt.plot(sierraleone_df['Timestamp'], sierraleone_df['GHI'], label='Sierra Leone GHI')
+plt.legend()
+
+plt.subplot(3, 1, 3)
+plt.plot(togo_df['Timestamp'], togo_df['GHI'], label='Togo GHI')
+plt.legend()
+
+plt.show()
+
 
 import matplotlib.pyplot as plt
 import seaborn as sns
